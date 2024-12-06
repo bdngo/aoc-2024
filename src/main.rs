4064e7223ff022495427d0a59a1b62b58a062bc4
Bryan Ngo <bryanngo@berkeley.edu>
2024-12-06 12:40:32 -0800

    finish day 6 part 1

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ pub mod day02;
 pub mod day03;
 pub mod day04;
 pub mod day05;
+pub mod day06;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let fname = &args[1];
     let contents = fs::read_to_string(fname)?;
 
-    let solution = day05::part2(contents);
+    let solution = day06::part1(contents);
     println!("{}", solution);
 
     Ok(())
