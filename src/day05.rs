0cc803fe82586509c2e79ca591f6b27503e3bb37
Bryan Ngo <bryanngo@berkeley.edu>
2024-12-06 11:15:29 -0800

    complete day 5 part 2

--- src/day05.rs
+++ src/day05.rs
@@ -1,3 +1,5 @@
+use std::{cmp::Ordering, collections::HashMap};
+
 #[derive(Clone, Debug)]
 struct Rule {
     before_page: u64,
@@ -51,6 +53,8 @@ fn get_middle_element(vec: &Vec<u64>) -> u64 {
     vec[middle_index]
 }
 
+type RuleGraph = HashMap<u64, Vec<u64>>;
+
 pub fn part2(input: String) -> u64 {
     if let [rules, updates] = &input.split("\n\n").collect::<Vec<_>>()[..] {
         let rules_list: Vec<Rule> = rules
@@ -74,55 +78,41 @@ pub fn part2(input: String) -> u64 {
                 .filter_map(|y| y.parse::<u64>().ok())
                 .collect::<Vec<u64>>()
         });
+        let rule_graph = create_rule_graph(&rules_list);
         let incorrect_updates = update_list.filter(|x| !is_valid_update(x, &rules_list));
-        let mut sum = 0;
-        for mut bad_update in incorrect_updates.clone() {
-            let (mut corrections, mut other_rules) =
-                get_corrections(&bad_update, rules_list.clone());
-            while !corrections.is_empty() {
-                // correct update
-                let correction = corrections.pop().unwrap();
-                if let (Some(before_idx), Some(after_idx)) = (
-                    bad_update.iter().position(|&x| x == correction.before_page),
-                    bad_update.iter().position(|&x| x == correction.after_page),
-                ) {
-                    bad_update.swap(before_idx, after_idx);
-                }
-                // println!("{:?}", bad_update);
-
-                // add potentially new violating rules
-                let mut to_remove = Vec::new();
-                for (i, rule) in other_rules.iter().enumerate() {
-                    if let (Some(before_idx), Some(after_idx)) = (
-                        bad_update.iter().position(|&x| x == rule.before_page),
-                        bad_update.iter().position(|&x| x == rule.after_page),
-                    ) {
-                        // println!("{:?}", (before_idx, after_idx));
-                        if before_idx > after_idx {
-                            to_remove.push(i);
-                            corrections.push(rule.clone());
+        return incorrect_updates
+            .map(|mut bad_update| {
+                bad_update.sort_by(|y, z| {
+                    if let Some(rule_edges) = rule_graph.get(y) {
+                        if rule_edges.contains(z) {
+                            return Ordering::Less;
+                        }
+                    } else if let Some(rule_edges) = rule_graph.get(z) {
+                        if rule_edges.contains(y) {
+                            return Ordering::Greater;
                         }
                     }
-                }
-                to_remove.reverse();
-                for i in to_remove {
-                    other_rules.remove(i);
-                }
-            }
-            sum += get_middle_element(&bad_update);
-        }
-        return sum;
+                    Ordering::Equal
+                });
+                bad_update
+            })
+            .map(|x| get_middle_element(&x))
+            .sum();
     }
     0
 }
 
-fn get_corrections(update: &Vec<u64>, rules: Vec<Rule>) -> (Vec<Rule>, Vec<Rule>) {
-    rules.into_iter().partition(|x| {
-        if update.contains(&x.before_page) && update.contains(&x.after_page) {
-            update.iter().position(|&y| y == x.before_page)
-                > update.iter().position(|&y| y == x.after_page)
-        } else {
-            false
-        }
-    })
+fn create_rule_graph(rules_list: &Vec<Rule>) -> RuleGraph {
+    let mut rule_graph = RuleGraph::new();
+    for Rule {
+        before_page,
+        after_page,
+    } in rules_list
+    {
+        rule_graph
+            .entry(*before_page)
+            .or_insert(Vec::new())
+            .push(*after_page);
+    }
+    rule_graph
 }
