94f1281c411b90f650a12e6530f4d75756a3f7d8
Bryan Ngo <bryanngo@berkeley.edu>
2024-12-07 20:29:50 -0800

    redo day 7 recursively

--- src/day07.rs
+++ src/day07.rs
@@ -1,18 +1,9 @@
-use bitvec::{prelude::BitVec, view::BitView};
-use itertools::Itertools;
-use std::collections::VecDeque;
+use std::cmp::Ordering;
 
 #[derive(Debug, Clone)]
 struct Equation {
     total: u64,
     operands: Vec<u64>,
-}
-
-#[derive(Debug, Clone)]
-enum Operator {
-    Add,
-    Multiply,
-    Concat,
 }
 
 pub fn part1(input: String) -> u64 {
@@ -35,36 +26,40 @@ pub fn part1(input: String) -> u64 {
     equations
         .iter()
         .filter(|&equation| {
-            for i in 0usize..1 << equation.operands.len() - 1 {
-                let operator_bitvec: BitVec = i.view_bits().to_bitvec();
-                let operator_list = operator_bitvec
-                    .into_iter()
-                    .map(|b| if b { Operator::Multiply } else { Operator::Add })
-                    .collect();
-                if evaluate_equation(equation, operator_list) == equation.total {
-                    return true;
-                }
-            }
-            false
+            let mut mut_equation = equation.clone();
+            mut_equation.operands.reverse();
+            evaluate_equation_binary(mut_equation)
         })
         .map(|x| x.total)
         .sum()
 }
 
-fn evaluate_equation(equation: &Equation, operators: Vec<Operator>) -> u64 {
-    let init_elem = VecDeque::from(equation.operands.clone())
-        .pop_front()
-        .unwrap_or_default();
-    equation
-        .operands
-        .iter()
-        .skip(1)
-        .zip(operators)
-        .fold(init_elem, |x, (y, z)| match z {
-            Operator::Multiply => x * y,
-            Operator::Add => x + y,
-            Operator::Concat => x * 10u64.pow(y.ilog10() + 1) + y,
-        })
+fn evaluate_equation_binary(mut equation: Equation) -> bool {
+    if let Some(leftmost) = equation.operands.pop() {
+        match leftmost.cmp(&equation.total) {
+            Ordering::Equal => return true,
+            Ordering::Greater => return false,
+            Ordering::Less => {
+                if let Some(second_elem) = equation.operands.pop() {
+                    let mut test_mul = equation.operands.clone();
+                    let mut test_add = equation.operands.clone();
+                    test_mul.push(leftmost + second_elem);
+                    test_add.push(leftmost * second_elem);
+                    return evaluate_equation_binary(Equation {
+                        total: equation.total,
+                        operands: test_add,
+                    }) || evaluate_equation_binary(Equation {
+                        total: equation.total,
+                        operands: test_mul,
+                    });
+                } else {
+                    return false;
+                }
+            }
+        }
+    } else {
+        false
+    }
 }
 
 pub fn part2(input: String) -> u64 {
@@ -87,20 +82,43 @@ pub fn part2(input: String) -> u64 {
     equations
         .iter()
         .filter(|&equation| {
-            let mut possible_operators = (0..equation.operands.len() - 1)
-                .map(|_| {
-                    (0..3).map(|x| match x {
-                        0 => Operator::Add,
-                        1 => Operator::Multiply,
-                        2 => Operator::Concat,
-                        _ => unreachable!(),
-                    })
-                })
-                .multi_cartesian_product();
-            // println!("{:?}", evaluate_equation(equation, vec![Operator::Concat]));
-            possible_operators
-                .any(|operations| evaluate_equation(equation, operations) == equation.total)
+            let mut mut_equation = equation.clone();
+            mut_equation.operands.reverse();
+            evaluate_equation_ternary(mut_equation)
         })
         .map(|x| x.total)
         .sum()
+}
+
+fn evaluate_equation_ternary(mut equation: Equation) -> bool {
+    if let Some(leftmost) = equation.operands.pop() {
+        match leftmost.cmp(&equation.total) {
+            Ordering::Equal => return true,
+            Ordering::Greater => return false,
+            Ordering::Less => {
+                if let Some(second_elem) = equation.operands.pop() {
+                    let mut test_mul = equation.operands.clone();
+                    let mut test_add = equation.operands.clone();
+                    let mut test_concat = equation.operands.clone();
+                    test_mul.push(leftmost + second_elem);
+                    test_add.push(leftmost * second_elem);
+                    test_concat.push(leftmost * 10u64.pow(second_elem.ilog10() + 1) + second_elem);
+                    return evaluate_equation_ternary(Equation {
+                        total: equation.total,
+                        operands: test_add,
+                    }) || evaluate_equation_ternary(Equation {
+                        total: equation.total,
+                        operands: test_mul,
+                    }) || evaluate_equation_ternary(Equation {
+                        total: equation.total,
+                        operands: test_concat,
+                    });
+                } else {
+                    return false;
+                }
+            }
+        }
+    } else {
+        false
+    }
 }
